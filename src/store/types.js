57625c1134a36ff756f41b7fe2c60a29165f4912
Steve Edwards <steveedwards-ctr@iwsinc.com>
2020-05-21 08:25:53 -0700

    Add missing types in types.js

--- src/store/types.js
+++ src/store/types.js
@@ -1,3 +1,5 @@
+export const SET_USER = 'SET_USER';
+export const SET_LOGGED_IN = 'SET_LOGGED_IN';
 export const SET_ROLES = 'SET_ROLES';
 export const SET_FORMS = 'SET_FORMS';
 export const SET_USER_ROLES = 'SET_USER_ROLES';
